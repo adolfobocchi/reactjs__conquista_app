c6394e3133c109de37344c56874be2a92c46552a
professoradolfobocchi <professoradolfobocchi@outlook.com>
2022-06-29 21:47:22 -0300

    first commit

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -8,9 +8,7 @@ function Description() {
                     um mundo de aventuras
                 </div>
                 <div class="description-content">
-                    convide seus amigos pra um momento de
-                    diversão nesse mundo de aventuras
-                    cançando os monstros
+                Treine seus reflexos neste incrível jogo estilo clique coletando o maior número de itens possíveis e evitando clicar nas bombas e caveiras.
                     <div class="description-btn">
                         <a href="http://itch.io/" target="_blank">DOWNLOAD</a>
                     </div>
